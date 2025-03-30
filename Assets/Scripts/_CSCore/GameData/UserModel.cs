08b562b9dceb15a26b2d0666c1757162acda64a9
huyngo0211 <huynq.aord@gmail.com>
2025-03-30 18:18:34 +0700

    s

--- Assets/Scripts/_CSCore/GameData/UserModel.cs
+++ Assets/Scripts/_CSCore/GameData/UserModel.cs
@@ -8,4 +8,18 @@ public class UserModel
     public string UserDisplayName { get; set; }
     public string UserEmail { get; set; }
     public string UserUrlAvatar { get; set; }
+
+    public int AvatarUsingId { get; set; } = -1;
+    //1 la avatar mac dinh khi tao nich, gg thi la avat url gg, fb thi la avat url fb, con lai dung avt mac dinh cua he thong
+    public List<int> OwnAvatars { get; set; } = new List<int>() {1};
+    //Character mac dinh dau game la 1
+    public int CharacterUsingId { get; set; } = 1;
+    public List<int> OwnCharacters { get; set; } = new List<int>() {1};
+    //Bullet mac dinh dau game la 1
+    public int BulletUsingId { get; set; } = 1;
+    public List<int> OwnBullets { get; set; } = new List<int>() {1};
+    //
+    public long Coin { get; set; }
+    public long Gem { get; set; }
+    public int CurrentMapLevel { get; set; } = 1;
 }
